4261c9fc98ff08c83ac4deaa507c2bc58fd0241f
claire chen <clairewenhuang.chen@gmail.com>
2018-12-19 13:26:01 +0800

    add untracked files

---
+++ pages/landing/landing.js
@@ -0,0 +1,79 @@
+// pages/landing/landing.js
+
+Page({
+  onTap: function (event) {
+        wx.redirectTo({
+            url:"../index/index"
+        })
+  },
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    // login: function(e) {
+    //   wx.redirectTo({
+    //     url: 'pages/feebacks/feebacks',
+    //   })
+
+    // }
+  },
+
+  
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
+
